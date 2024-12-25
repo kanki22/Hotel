1bf6f7170adf1488b37055e942716ab667230ab2
kanki22 <156949498+kanki22@users.noreply.github.com>
2024-12-25 23:31:34 +0300

    Update Otel.cs

--- Otel.cs
+++ Otel.cs
@@ -29,87 +29,83 @@ namespace Otel{
             }
             
         }
-        class Odeme : Rezervasyon{   
-            public int fiyat {get; set;} = 2000;
-            public int hesapla(){ 
-               return fiyat *= Gun;
+   class Odeme : Rezervasyon{   
+         public int fiyat {get; set;} = 2000;
+         public int hesapla(){ 
+         return fiyat *= Gun;
             }
 
             
 
-        }
-        class Kisi : Odeme{
-            public static Kisi[] musteriler {get; set;} = new Kisi[daireSayisi];
-            public static int musteriSayisi {get; set;} = 0;
+   }
+   class Kisi : Odeme{
+         public static Kisi[] musteriler {get; set;} = new Kisi[daireSayisi];
+         public static int musteriSayisi {get; set;} = 0;
 
-            public static void rezerv (int daireNumara, string isim, string telefon, int gun){
+         public static void rezerv (int daireNumara, string isim, string telefon, int gun){
 
-                  if (daireNumara < 1 || daireNumara > 16){
-                     Console.WriteLine("Daire 1 - 16 arasinda olmasi gerekir");         //? eger yanlis numara girirldi
-                     return;
-                  }
-                  else if ( daireDurumu[ daireNumara-1 ] ){                             //? eger daire onceden alinmis
-                     Console.WriteLine("Daire doludur..");
-                     return;
-                  }
-                  else if ( gun > 7 ){
-                     Console.WriteLine("7 gun ustu rezervasyon edemezsiniz");
-                     return;
-                  }
+               if (daireNumara < 1 || daireNumara > 16){
+                  Console.WriteLine("Daire 1 - 16 arasinda olmasi gerekir");         //? eger yanlis numara girirldi
+                  return;
+               }
+               else if ( daireDurumu[ daireNumara-1 ] ){                             //? eger daire onceden alinmis
+                  Console.WriteLine("Daire doludur..");
+                  return;
+               }
+               else if ( gun > 7 ){
+                  Console.WriteLine("7 gun ustu rezervasyon edemezsiniz");
+                  return;
+               }
                   
-                  Oda musteri = new Oda(daireNumara, isim, telefon, gun);
+               Oda musteri = new Oda(daireNumara, isim, telefon, gun);
                   
-                  musteriler[musteriSayisi] = musteri;
-                  musteriSayisi++;
+               musteriler[musteriSayisi] = musteri;
+               musteriSayisi++;
 
-                  daireDurumu[daireNumara-1] = true;   //? Daireleri doldurma islemi
-                  daireSayisi--;                       //? daire alindiktan sonra daire sayisini azaltiyoruz
+               daireDurumu[daireNumara-1] = true;   //? Daireleri doldurma islemi
+               daireSayisi--;                       //? daire alindiktan sonra daire sayisini azaltiyoruz
 
-                  int fiyat = musteri.hesapla();       //? fiyat hesaplama
-                  Console.WriteLine("Toplam : "+fiyat+"TL");
-                  Console.WriteLine("Daire "+daireNumara+" basariyla "+isim+" tarafindan rezerv edilmistir.");
-            }
-
-
-        }
+               int fiyat = musteri.hesapla();       //? fiyat hesaplama
+               Console.WriteLine("Toplam : "+fiyat+"TL");
+               Console.WriteLine("Daire "+daireNumara+" basariyla "+isim+" tarafindan rezerv edilmistir.");
+         }
+   }
         
-        class Oda : Kisi{
-            
-            public Oda(int daireNumara, string isim, string telefon, int gun){                                             //? constructure daire rezervasyonu
-               
+   class Oda : Kisi{     
+         public Oda(int daireNumara, string isim, string telefon, int gun){                //? constructure daire rezervasyonu    
                this.daireNumara = daireNumara;      //? isimler ayni oldu icin this keywordu kullaniyoruz
                this.isim = isim;            //? isimler ayni oldu icin this keywordu kullaniyoruz
                this.telefon = telefon;            //? isimler ayni oldu icin this keywordu kullaniyoruz
                this.gun = gun;
-            }
-            public static void goster(){
+         }
+         public static void goster(){
    
                Console.WriteLine("\nMusterilerin bilgileri\n");
                for(int i=0; i<musteriSayisi; i++){
-                  Console.WriteLine("\n"+(i+1)+". Musteri kayidi : \n");
-                  var musteri = musteriler[i];
+                     Console.WriteLine("\n"+(i+1)+". Musteri kayidi : \n");
+                     var musteri = musteriler[i];
 
-                  Console.WriteLine("Musteri Adi : "+musteri.isim+"\nMusteri cep telefonu : "+musteri.telefon);
-                  Console.WriteLine("Daire : "+musteri.daireNumara+"\nSure : "+musteri.gun+" gun");
+                     Console.WriteLine("Musteri Adi : "+musteri.isim+"\nMusteri cep telefonu : "+musteri.telefon);
+                     Console.WriteLine("Daire : "+musteri.daireNumara+"\nSure : "+musteri.gun+" gun");
                }
-            }
+         }
             
-            public static int bosDaireSayisi(){
+         public static int bosDaireSayisi(){
                return daireSayisi;                                                //? islemlerden sonra bos daire sayisi
-            }
+         }
 
-        }
+   }
         
     
-        static void Main(){
+   static void Main(){
 
-            string isim;
-            int gun;
-            string telefon;
-            int daire;
+         string isim;
+         int gun;
+         string telefon;
+         int daire;
             
             
-            while( true ){
+         while( true ){
                Console.Write("\nOtelimize Hosgeldiniz \n");
 
                Console.Write("isim soyisim : ");
@@ -135,15 +131,10 @@ namespace Otel{
                }
                
              
-            }
-            Oda.goster();
-            Oda.bosDaireSayisi();
-
-            //Console.WriteLine(musteri1.Gun+" gundur");
-            //Console.WriteLine("fiyat = "+musteri1.Fiyat);
-
-
-        }
-        
+         }
+         Oda.goster();
+         Oda.bosDaireSayisi();
    }
+        
+  }
 }
